44ae9e7064d7d5f3f84cd69f0b788f3684687140
Oleg <oleh.skoropadskiy@megadevllc.com>
2020-09-01 17:27:54 +0300

    This!!!!

--- JS+React/script.js
+++ JS+React/script.js
@@ -155,26 +155,123 @@
 
 
 //Функции-консткрукторы
-function User(name, id) {
-    this.name = name;
-    this.id = id;
-    this.human = true;
-    this.hello = function () {
-        console.log(`Hello ${this.name}`);
-    }
-}
+// function User(name, id) {
+//     this.name = name;
+//     this.id = id;
+//     this.human = true;
+//     this.hello = function () {
+//         console.log(`Hello ${this.name}`);
+//     }
+// }
+//
+// User.prototype.exit = function () {
+//     console.log(`Пользователь ${this.name} ушел`);
+// };
+//
+// const oleg = new User('Oleg', 26);
+// const alex = new User('Alex', 20);
+//
+// oleg.exit();
+//
+// oleg.hello();
+// alex.hello();
+//
+// console.log(oleg);
+// console.log(alex);
 
-User.prototype.exit = function () {
-    console.log(`Пользователь ${this.name} ушел`);
-};
 
-const oleg = new User('Oleg', 26);
-const alex = new User('Alex', 20);
 
-oleg.exit();
+//This
 
-oleg.hello();
-alex.hello();
+// function showThis(a, b) {
+//     console.log(this);
+//     function sum() {
+//         console.log(this);
+//         return a + b;
+//     }
+//
+//     console.log(sum())
+// }
+// showThis(4, 5);
 
-console.log(oleg);
-console.log(alex);
+// const obj = {
+//     a: 20,
+//     b: 15,
+//     sum: function () {
+//         function shout() {
+//             console.log(this);
+//         }
+//         shout();
+//     }
+// };
+// obj.sum();
+
+// function User(name, id) {
+//     this.name = name;
+//     this.id = id;
+//     this.human = true;
+//     this.hello = function() {
+//         console.log("Hello! " + this.name);
+//     }
+// }
+// let oleg = new User('Oleg', 26);
+
+// function sayName(surname) {
+//     console.log(this);
+//     console.log(this.name + surname);
+// }
+//
+// const user = {
+//     name: 'John '
+// };
+//
+// sayName.call(user, 'Smith');
+// sayName.apply(user, ['Smith']);
+//
+// function count(num) {
+//     return this * num;
+// }
+//
+// const double = count.bind(2);
+// console.log(double(3));
+// console.log(double(31));
+
+// 1) Обычная функция: this = window, но если use strict - undefined
+// 2) Контексту методов обьекта - сам объект
+// 3) this в конструкторах и классах - это новый экземпляр объекта
+// 4) Ручная привязка this: call, apply, bind
+
+// const btn = document.querySelector('button');
+//
+// btn.addEventListener('click',  (e) => {
+//     e.target.style.backgroundColor = 'red';
+// });
+//
+// const obj = {
+//   num: 5,
+//   sayNumber: function () {
+//       const say = () => {
+//           console.log(this);
+//       };
+//
+//       say();
+//   }
+// };
+// obj.sayNumber();
+//
+// const double = (a, b) => a * 2;
+//
+// console.log(double(4))
+
+
+
+
+
+
+
+
+
+
+
+
+
