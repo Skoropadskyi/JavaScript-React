8c8622c53fb6a139ec724c0edd66a5d6dd19e339
Oleg <oleh.skoropadskiy@megadevllc.com>
2020-09-01 14:34:17 +0300

    Функции конструктор.

--- JS+React/script.js
+++ JS+React/script.js
@@ -152,3 +152,29 @@
 // let end = new Date();
 //
 // alert(`Цикл отработал за ${end - start} миллисекунд`);
+
+
+//Функции-консткрукторы
+function User(name, id) {
+    this.name = name;
+    this.id = id;
+    this.human = true;
+    this.hello = function () {
+        console.log(`Hello ${this.name}`);
+    }
+}
+
+User.prototype.exit = function () {
+    console.log(`Пользователь ${this.name} ушел`);
+};
+
+const oleg = new User('Oleg', 26);
+const alex = new User('Alex', 20);
+
+oleg.exit();
+
+oleg.hello();
+alex.hello();
+
+console.log(oleg);
+console.log(alex);
