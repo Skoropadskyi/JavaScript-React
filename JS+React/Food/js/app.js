9c3c6b66a5a2cce5547f7a807f66d5e19fca553b
Oleg <oleh.skoropadskiy@megadevllc.com>
2020-09-01 12:21:47 +0300

    Создаем модальное окно.

--- JS+React/Food/js/app.js
+++ JS+React/Food/js/app.js
@@ -41,7 +41,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
     //Timer
 
-    const deadLine = '2020-09-02';
+    const deadLine = '2020-12-31';
 
     function getTimeRemaining(endtime) {
         const t = Date.parse(endtime) - Date.parse(new Date()),
@@ -91,4 +91,38 @@ window.addEventListener('DOMContentLoaded', () => {
         }
     }
     setClock('.timer', deadLine);
+
+    //Modal
+
+    const modalTrigger = document.querySelectorAll('[data-modal]'),
+        modal = document.querySelector('.modal'),
+        modalCloseBtn = document.querySelector('[data-close]');
+
+    modalTrigger.forEach(btn => {
+        btn.addEventListener('click', () => {
+            modal.classList.add('show');
+            modal.classList.remove('hide');
+            document.body.style.overflow = 'hidden';
+        });
+    });
+
+    function closeModal() {
+        modal.classList.add('hide');
+        modal.classList.remove('show');
+        document.body.style.overflow = '';
+    }
+
+    modalCloseBtn.addEventListener('click', closeModal);
+
+    modal.addEventListener('click', (e) => {
+        if (e.target === modal) {
+           closeModal();
+        }
+    });
+
+    document.addEventListener('keydown', (e) => {
+       if (e.code === "Escape" && modal.classList.contains('show')) {
+           closeModal();
+       }
+    });
 });
