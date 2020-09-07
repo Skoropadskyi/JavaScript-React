c2ec5da56c91a9c3e786dfdcff591d8f5174fd2a
Oleg <oleh.skoropadskiy@megadevllc.com>
2020-09-07 12:02:59 +0300

    Классы в работе.

--- JS+React/script.js
+++ JS+React/script.js
@@ -267,33 +267,33 @@
 
 //Классы (ES6)
 
-class Rectangle {
-    constructor(height, width) {
-        this.height = height;
-        this.width = width;
-    }
-
-    calcArea() {
-        return this.height * this.width;
-    }
-}
-
-class ColorRectangleWithText extends Rectangle {
-    constructor(height, width, text, bgColor) {
-        super(height, width);
-        this.text = text;
-        this.bgColor = bgColor;
-    }
-
-    showMyProps() {
-        console.log(`Текст: ${this.text}, цвет: ${this.bgColor}`);
-    }
-}
-
-const div = new ColorRectangleWithText(25, 10, 'Hello World!', 'red');
-
-div.showMyProps();
-console.log(div.calcArea());
+// class Rectangle {
+//     constructor(height, width) {
+//         this.height = height;
+//         this.width = width;
+//     }
+//
+//     calcArea() {
+//         return this.height * this.width;
+//     }
+// }
+//
+// class ColorRectangleWithText extends Rectangle {
+//     constructor(height, width, text, bgColor) {
+//         super(height, width);
+//         this.text = text;
+//         this.bgColor = bgColor;
+//     }
+//
+//     showMyProps() {
+//         console.log(`Текст: ${this.text}, цвет: ${this.bgColor}`);
+//     }
+// }
+//
+// const div = new ColorRectangleWithText(25, 10, 'Hello World!', 'red');
+//
+// div.showMyProps();
+// console.log(div.calcArea());
 
 // const square = new Rectangle(10, 10);
 // const long = new Rectangle(20, 100);
