343e473a34157791bdcdb76f1ba18aa98a78d253
Oleg <oleh.skoropadskiy@megadevllc.com>
2020-09-07 12:24:47 +0300

    Rest

--- JS+React/script.js
+++ JS+React/script.js
@@ -303,7 +303,18 @@
 
 
 
+// Rest
 
-
+// const log = function (a, b, ...rest) {
+//     console.log(a, b, rest);
+// }
+//
+// log('basic', 'rest', 'operator', 'usage');
+//
+// function calcOrDouble(number, basis = 2) {
+//     console.log(number * basis);
+// }
+//
+// calcOrDouble(3);
 
 
