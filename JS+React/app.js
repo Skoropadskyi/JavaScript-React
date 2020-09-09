192bd1f885b4b5ebd6ecac3c67f052df8cd12024
Oleg <oleh.skoropadskiy@megadevllc.com>
2020-09-09 16:03:25 +0300

    JSON

---
+++ JS+React/app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+// JSON
+
+// const persone = {
+//     name: 'Alex',
+//     tel: '+74444444',
+//     parents: {
+//         mom: 'Olga',
+//         dad: 'Mike'
+//     }
+// };
+//
+// const clone = JSON.parse(JSON.stringify(persone));
+// clone.parents.mom = 'Ann';
+//
+// console.log(persone);
+// console.log(clone);
