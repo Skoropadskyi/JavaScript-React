0aff8a54a91038b988811268aff6534d77e42c5d
Oleg <oleh.skoropadskiy@megadevllc.com>
2020-09-15 16:18:58 +0300

    JSON-server

--- JS+React/Food/js/app.js
+++ JS+React/Food/js/app.js
@@ -290,6 +290,10 @@ window.addEventListener('DOMContentLoaded', () => {
         },4000);
     }
 
+    fetch('http://localhost:3000/menu')
+        .then(data => data.json())
+        .then(res => console.log(res));
+
 });
 
 
