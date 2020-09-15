58b16e8fb1bf2abc3b9314b743b9c0331664fd0f
Oleg <oleh.skoropadskiy@megadevllc.com>
2020-09-15 14:33:57 +0300

    Filter, map, reduce, every, some, obj.entries.

--- JS+React/app.js
+++ JS+React/app.js
@@ -71,3 +71,60 @@
 // Promise.race([test(1000), test(1000)]).then(() => {
 //     console.log('All');
 // });
+
+
+
+// Filter
+
+// const names = ['Ivan', 'Ann', 'Ksenia', 'Voldemart'];
+//
+// const shortNames = names.filter((name) => {
+//     return name.length < 5;
+// });
+//
+// console.log(shortNames);
+
+// Map
+
+// const answers = ['IvAn', 'AnnA', 'Hello'];
+//
+// const result = answers.map(item => item.toLowerCase());
+//
+// console.log(result);
+
+// every / some
+
+// const some = [4, 'qwer', 'puk'];
+//
+// console.log(some.some(item => typeof (item) === 'number'));
+//
+// console.log(some.every(item => typeof (item) === 'number'));
+
+// Reduce
+
+// const arr = [4, 5, 1, 3, 2, 6];
+//
+// const res = arr.reduce((sum, current) => sum + current);
+//
+// console.log(res);
+
+// const arr = ['apple', 'pear', 'plum'];
+//
+// const res = arr.reduce((sum, current) => `${sum}, ${current}`);
+//
+// console.log(res);
+
+// const obj = {
+//   ivan: 'persone',
+//   ann: 'persone',
+//   dog: 'animal',
+//   cat: 'animal'
+// };
+//
+// const newArr = Object.entries(obj)
+//     .filter(item => item[1] === 'persone')
+//     .map(item => item[0]);
+//
+// console.log(newArr);
+
+
