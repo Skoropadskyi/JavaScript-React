7d99ae8231d28e1bf887f10e9ed392921141f74c
Oleg <oleh.skoropadskiy@megadevllc.com>
2020-09-29 11:46:15 +0300

    requestAnimationFrame

--- JS+React/SetInterval/script.js
+++ JS+React/SetInterval/script.js
@@ -1,24 +1,37 @@
-const btn = document.querySelector('.btn');
-let timerId;
-    i= 0;
+const btn = document.querySelector('.btn'),
+     elem = document.querySelector('.box');
+let pos = 0;
 
-    function myAnimation() {
-        const elem = document.querySelector('.box');
-        let pos = 0;
+    // function myAnimation() {
+    //     let pos = 0;
+    //
+    //     const id = setInterval(frame, 10);
+    //     function frame() {
+    //         if (pos === 300) {
+    //             clearInterval(id);
+    //         } else {
+    //             pos++;
+    //             elem.style.top = pos + 'px';
+    //             elem.style.left = pos + 'px';
+    //         }
+    //     }
+    // }
 
-        const id = setInterval(frame, 10);
-        function frame() {
-            if (pos === 300) {
-                clearInterval(id);
-            } else {
-                pos++;
-                elem.style.top = pos + 'px';
-                elem.style.left = pos + 'px';
-            }
-        }
+function myAnimation() {
+    pos++;
+    elem.style.top = pos + 'px';
+    elem.style.left = pos + 'px';
+
+    if (pos < 300) {
+        requestAnimationFrame(myAnimation);
     }
+}
 
-btn.addEventListener('click', myAnimation);
+btn.addEventListener('click', () => requestAnimationFrame(myAnimation));
+
+let id = requestAnimationFrame(myAnimation);
+cancelAnimationFrame(id);
+
 
 // function logger () {
 //     if (i === 3) {
